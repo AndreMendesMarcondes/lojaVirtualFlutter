0fba6f47f3638aafa9e83b6f58f2aae1d5b63e57
7700508659 <andre.marcondes-ext@viavarejo.com.br>
2020-06-06 17:40:37 -0300

    sigin implementation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,12 +3,21 @@ import 'package:lojavirtual/models/user_model.dart';
 import 'package:lojavirtual/screens/signup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Entrar"),
         centerTitle: true,
@@ -20,17 +29,17 @@ class LoginScreen extends StatelessWidget {
             ),
             textColor: Colors.white,
             onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context)=>SignUpScreen())
-              );
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => SignUpScreen()));
             },
           )
         ],
       ),
       body: ScopedModelDescendant<UserModel>(
-        builder: (context, child, model){
-          if(model.isLoading)
-            return Center(child: CircularProgressIndicator(),);
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
 
           return Form(
             key: _formKey,
@@ -38,17 +47,18 @@ class LoginScreen extends StatelessWidget {
               padding: EdgeInsets.all(16),
               children: <Widget>[
                 TextFormField(
+                  controller: _emailController,
                   decoration: InputDecoration(hintText: "E-mail"),
                   keyboardType: TextInputType.emailAddress,
                   validator: (text) {
-                    if (text.isEmpty || !text.contains("@"))
-                      return "E-mail inválido!";
+                    if (text.isEmpty || !text.contains("@")) return "E-mail inválido!";
                   },
                 ),
                 SizedBox(
                   height: 16,
                 ),
                 TextFormField(
+                  controller: _passController,
                   decoration: InputDecoration(hintText: "Senha"),
                   obscureText: true,
                   validator: (text) {
@@ -80,7 +90,11 @@ class LoginScreen extends StatelessWidget {
                     color: Theme.of(context).primaryColor,
                     onPressed: () {
                       if (_formKey.currentState.validate()) {}
-                      model.signIn();
+                      model.signIn(
+                          email: _emailController.text,
+                          pass: _passController.text,
+                          onSuccess: _onSuccess,
+                          onFail: _onFail);
                     },
                   ),
                 )
@@ -90,5 +104,17 @@ class LoginScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _onSuccess() {
+    Navigator.of(context).pop();
+  }
+
+  void _onFail() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Falha ao entrar!"),
+      backgroundColor: Colors.redAccent,
+      duration: Duration(seconds: 2),
+    ));
   }
 }
