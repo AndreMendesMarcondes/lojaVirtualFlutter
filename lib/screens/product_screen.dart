4286373af36c21dc2416a815668f29694bf38dd8
7700508659 <andre.marcondes-ext@viavarejo.com.br>
2020-06-07 14:41:20 -0300

    order status follow

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/datas/cart_product.dart';
 import 'package:lojavirtual/datas/product_data.dart';
@@ -110,6 +111,7 @@ class _ProductScreenState extends State<ProductScreen> {
                               cartProduct.quantity = 1;
                               cartProduct.pid = product.id;
                               cartProduct.category = product.category;
+                              cartProduct.productData = product;
                               CartModel.of(context).addCartItem(cartProduct);
 
                               Navigator.of(context).push(MaterialPageRoute(builder: (context) => CartScreen()));
